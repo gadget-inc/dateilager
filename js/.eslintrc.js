7f5438874610959fe9dcbdac18c0ea8fa314ad11
Alex Angelini <alex.louis.angelini@gmail.com>
2022-05-24 14:04:01 -0400

    Backport telemetry changes

---
+++ js/.eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: "@gadgetinc/eslint-config",
+};
--- js/src/exec.ts
+++ js/src/exec.ts
@@ -5,6 +5,7 @@ import fse from "fs-extra";
 import pMemoize from "p-memoize";
 import type { Logger } from "pino";
 import readline from "readline";
+import { traced } from "./telemetry";
 
 const DL_UPDATE_TIMEOUT = 10000;
 const DL_REBUILD_TIMEOUT = 8000;
@@ -20,27 +21,18 @@ export class DateiLagerBinaryClient {
   memoizedTokenFn: () => Promise<string>;
   tracing: boolean;
 
-  constructor(
-    logger: Logger,
-    host: string,
-    port: number,
-    tokenFn: () => Promise<string>,
-    tracing: boolean
-  ) {
+  constructor(logger: Logger, host: string, port: number, tokenFn: () => Promise<string>, tracing: boolean) {
     this.logger = logger;
     this.server = `${host}:${port}`;
     this.memoizedTokenFn = pMemoize(tokenFn);
     this.tracing = tracing;
   }
 
+  @traced("dateilager-binary-client.update", {
+    record: (project, diff, directory) => ({ project, diff, directory }),
+  })
   async update(project: bigint, directory: string): Promise<bigint | null> {
-    const result = await this._call(
-      "update",
-      project,
-      directory,
-      DL_UPDATE_TIMEOUT,
-      ["-dir", directory]
-    );
+    const result = await this._call("update", project, directory, DL_UPDATE_TIMEOUT, ["-dir", directory]);
 
     if (result.stdout == "-1") {
       return null;
@@ -49,25 +41,17 @@ export class DateiLagerBinaryClient {
     return BigInt(result.stdout);
   }
 
-  async rebuild(
-    project: bigint,
-    from: bigint,
-    to: bigint | null,
-    directory: string
-  ): Promise<bigint | null> {
+  @traced("dateilager-binary-client.rebuild", {
+    record: (project, from, to, output) => ({ project, from, to, output }),
+  })
+  async rebuild(project: bigint, from: bigint, to: bigint | null, directory: string): Promise<bigint | null> {
     let args = ["-from", String(from), "-dir", directory];
     if (to) {
       args = args.concat(["-to", String(to)]);
     }
 
     await fse.mkdirp(directory);
-    const result = await this._call(
-      "rebuild",
-      project,
-      directory,
-      DL_REBUILD_TIMEOUT,
-      args
-    );
+    const result = await this._call("rebuild", project, directory, DL_REBUILD_TIMEOUT, args);
 
     if (result.stdout == "-1") {
       return null;
@@ -76,25 +60,9 @@ export class DateiLagerBinaryClient {
     return BigInt(result.stdout);
   }
 
-  async _call(
-    method: string,
-    project: bigint,
-    cwd: string,
-    timeout: number,
-    args: string[]
-  ): Promise<ExecaReturnValue<string>> {
+  async _call(method: string, project: bigint, cwd: string, timeout: number, args: string[]): Promise<ExecaReturnValue<string>> {
     const level = this.logger.level == "trace" ? "debug" : this.logger.level;
-    const baseArgs = [
-      method,
-      "-project",
-      String(project),
-      "-server",
-      this.server,
-      "-encoding",
-      "json",
-      "-log",
-      level,
-    ];
+    const baseArgs = [method, "-project", String(project), "-server", this.server, "-encoding", "json", "-log", level];
 
     if (this.tracing) {
       const carrier = {};
