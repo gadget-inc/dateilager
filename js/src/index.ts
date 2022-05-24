dc013a4cb02d8c7ae0200d2f1334941b1a33fa68
Alex Angelini <alex.louis.angelini@gmail.com>
2022-05-24 14:04:01 -0400

    Backport telemetry changes

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,33 +1,39 @@
 import { ChannelCredentials, credentials, Metadata } from "@grpc/grpc-js";
+import type { Span } from "@opentelemetry/api";
+import { context, propagation, trace } from "@opentelemetry/api";
 import { GrpcTransport } from "@protobuf-ts/grpc-transport";
-import type { ClientStreamingCall } from "@protobuf-ts/runtime-rpc";
+import type { ClientStreamingCall, RpcOptions } from "@protobuf-ts/runtime-rpc";
 import pMemoize from "p-memoize";
 import { TextDecoder, TextEncoder } from "util";
 import type { Objekt, Project, UpdateRequest, UpdateResponse } from "./fs";
 import { FsClient } from "./fs.client";
+import { traced, tracer } from "./telemetry";
 
 const MB = 1024 * 1024;
 
 class UpdateInputStream {
-  project: bigint;
-  call: ClientStreamingCall<UpdateRequest, UpdateResponse>;
-
-  constructor(project: bigint, call: ClientStreamingCall<UpdateRequest, UpdateResponse>) {
-    this.project = project;
-    this.call = call;
-  }
+  constructor(private project: bigint, private call: ClientStreamingCall<UpdateRequest, UpdateResponse>, private span: Span) {}
 
   send(obj: Objekt): Promise<void> {
-    return this.call.requests.send({
-      project: this.project,
-      object: obj,
-    });
+    try {
+      return this.call.requests.send({
+        project: this.project,
+        object: obj,
+      });
+    } catch (err) {
+      this.span.end();
+      throw err;
+    }
   }
 
   async complete(): Promise<bigint | null> {
-    await this.call.requests.complete();
-    const response = await this.call.response;
-    return response.version != -1n ? response.version : null;
+    try {
+      await this.call.requests.complete();
+      const response = await this.call.response;
+      return response.version != -1n ? response.version : null;
+    } finally {
+      this.span.end();
+    }
   }
 }
 
@@ -66,19 +72,19 @@ export class DateiLagerClient {
 
     const tokenMetaGenerator = (_params: any, callback: (err: Error | null, meta: Metadata) => void) => {
       memoizedTokenFn()
-          .then((token) => {
-            const meta = new Metadata();
-            meta.add("authorization", `Bearer ${token}`);
-            callback(null, meta);
-          })
-          .catch((error) => {
-            throw error;
-          });
+        .then((token) => {
+          const meta = new Metadata();
+          meta.add("authorization", `Bearer ${token}`);
+          callback(null, meta);
+        })
+        .catch((error) => {
+          throw error;
+        });
     };
 
     const creds = credentials.combineChannelCredentials(
-        ChannelCredentials.createSsl(rootCert),
-        credentials.createFromMetadataGenerator(tokenMetaGenerator)
+      ChannelCredentials.createSsl(rootCert),
+      credentials.createFromMetadataGenerator(tokenMetaGenerator)
     );
 
     this.transport = new GrpcTransport({
@@ -89,12 +95,17 @@ export class DateiLagerClient {
         "grpc.max_receive_message_length": 100 * MB,
       },
     });
+
     this.client = new FsClient(this.transport);
   }
 
-  _options() {
+  _options(): RpcOptions {
+    const meta = {};
+    propagation.inject(context.active(), meta);
+
     return {
       deadline: new Date().setSeconds(new Date().getSeconds() + 2),
+      meta,
     };
   }
 
@@ -102,51 +113,87 @@ export class DateiLagerClient {
     this.transport.close();
   }
 
+  @traced("dateilager-grpc-client.new-project", {
+    record: (project, pack_patterns, template) => ({
+      project,
+      pack_patterns,
+      template,
+    }),
+  })
   async newProject(project: bigint, packPatterns: string[], template?: bigint) {
     await this.client.newProject({ id: project, packPatterns: packPatterns, template: template }, this._options());
   }
 
+  @traced("dateilager-grpc-client.delete-project", {
+    record: (project) => ({ project }),
+  })
   async deleteProject(project: bigint): Promise<void> {
     await this.client.deleteProject({ project }, this._options());
   }
 
-  async *listObjects(project: bigint, path: string, ignores: string[] = []) {
-    const call = this.client.get(
-        {
-          project: project,
-          queries: [
-            {
-              path: path,
-              isPrefix: true,
-              withContent: true,
-              ignores: ignores,
-            },
-          ],
+  async *listObjects(project: bigint, path: string, ignores: string[] = [], from?: bigint, to?: bigint) {
+    const parentContext = context.active();
+    const span = tracer.startSpan(
+      "dateilager-grpc-client.list-objects",
+      {
+        attributes: {
+          "dl.project": project.toString(),
+          "dl.path": path,
+          "dl.ignores": ignores,
+          "dl.from_version": String(from),
+          "dl.to_version": String(to),
         },
-        this._options()
+      },
+      parentContext
     );
 
-    for await (const response of call.responses) {
-      if (response.object) {
-        yield response.object;
+    try {
+      const call = context.with(trace.setSpan(parentContext, span), () =>
+        this.client.get(
+          {
+            project: project,
+            fromVersion: from,
+            toVersion: to,
+            queries: [
+              {
+                path: path,
+                isPrefix: true,
+                withContent: true,
+                ignores: ignores,
+              },
+            ],
+          },
+          this._options()
+        )
+      );
+
+      for await (const response of call.responses) {
+        if (response.object) {
+          yield response.object;
+        }
       }
+    } finally {
+      span.end();
     }
   }
 
+  @traced("dateilager-grpc-client.get-object", {
+    record: (project, path) => ({ project, path }),
+  })
   async getObject(project: bigint, path: string): Promise<Objekt | undefined> {
     const call = this.client.get(
-        {
-          project: project,
-          queries: [
-            {
-              path: path,
-              isPrefix: false,
-              withContent: true,
-              ignores: [],
-            },
-          ],
-        },
-        this._options()
+      {
+        project: project,
+        queries: [
+          {
+            path: path,
+            isPrefix: false,
+            withContent: true,
+            ignores: [],
+          },
+        ],
+      },
+      this._options()
     );
 
     for await (const response of call.responses) {
@@ -155,8 +202,19 @@ export class DateiLagerClient {
   }
 
   updateObjects(project: bigint): UpdateInputStream {
-    const call = this.client.update(this._options());
-    return new UpdateInputStream(project, call);
+    const parentContext = context.active();
+    const span = tracer.startSpan(
+      "dateilager-grpc-client.update-objects",
+      {
+        attributes: {
+          "dl.project": project.toString(10),
+        },
+      },
+      parentContext
+    );
+
+    const call = context.with(trace.setSpan(parentContext, span), () => this.client.update(this._options()));
+    return new UpdateInputStream(project, call, span);
   }
 
   async updateObject(project: bigint, obj: Objekt): Promise<bigint | null> {
@@ -172,6 +230,10 @@ export class DateiLagerClient {
 
   async resetToSnapshotInDevOrTests(projects: Project[]): Promise<void> {
     await this.client.reset({ projects: projects }, this._options());
+  }
+
+  async resetToK8SSnapshot(): Promise<void> {
+    await this.client.reset({ projects: [{ id: 0n, version: 1n }] }, this._options());
   }
 
   async resetAllInDevOrTests(): Promise<void> {
