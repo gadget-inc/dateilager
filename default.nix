3d913b5f394807fa704a2f7918d14587cd7db8b0
Devon Boyer <devonboyer94@gmail.com>
2022-06-02 12:20:25 -0400

    fix vendorSha256

--- default.nix
+++ default.nix
@@ -17,7 +17,7 @@ buildGoModule rec {
   version = "0.1.0";
   src = ./.;
   proxyVendor = true; # Fixes: cannot query module due to -mod=vendor running make install
-  vendorSha256 = "sha256-cefXFcWh3J9uUjqfzdPjeagOyQe8MupGbKCfsP90+H4=";
+  vendorSha256 = "sha256-lmsCeUQOMoch9TI/rJjGEEeTfB3QmL2UsdeTFKBiGcc=";
 
   outputs = [ "out" "client" "server" "webui" "assets" "migrations" ];
 
