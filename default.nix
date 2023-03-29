216ae9e4be42beec1fcba84820d4ad59bee8be33
Alex Angelini <alex.louis.angelini@gmail.com>
2023-03-29 14:49:47 -0400

    Bump version to 0.5.7

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
 
 buildGoModule rec {
   pname = "dateilager";
-  version = "0.5.6";
+  version = "0.5.7";
   src = ./.;
   proxyVendor = true; # Fixes: cannot query module due to -mod=vendor running make install
   vendorSha256 = "sha256-PRjYm0gvSrSMjC/HnhTx5Jb7CY+WKzHCBXEoBwhUpAc=";
