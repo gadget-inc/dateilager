db5a2f3fadd9b05cc67af2d6c10fa23227463a87
Scott Côté <scott.cote@hey.com>
2023-05-02 15:16:51 -0400

    Bump version to 0.5.14

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
 
 buildGoModule rec {
   pname = "dateilager";
-  version = "0.5.13";
+  version = "0.5.14";
   src = ./.;
   proxyVendor = true; # Fixes: cannot query module due to -mod=vendor running make install
   vendorSha256 = "sha256-PRjYm0gvSrSMjC/HnhTx5Jb7CY+WKzHCBXEoBwhUpAc=";
