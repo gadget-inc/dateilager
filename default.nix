b12b3538c1343a204bf3853d27f8e8140959b3d1
Alex Angelini <alex.louis.angelini@gmail.com>
2024-07-10 12:30:47 +0200

    Bump version to 0.8.1

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
 
 buildGoModule rec {
   pname = "dateilager";
-  version = "0.8.0";
+  version = "0.8.1";
   src = ./.;
   proxyVendor = true; # Fixes: cannot query module due to -mod=vendor running make install
   vendorHash = "sha256-3XVou4NW28QxIe1t0gawbCoyl/3X22PoQdJOjcG0nDM=";
