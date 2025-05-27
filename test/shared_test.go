4203a741945cebb775916ac2fd90a4d583758e41
Scott Côté <scott.cote@hey.com>
2025-05-27 19:11:56 -0400

    Benchmark LVM

--- test/shared_test.go
+++ test/shared_test.go
@@ -976,3 +976,9 @@ func compareFileContents(info os.FileInfo, file1, file2 string) (bool, error) {
 
 	return bytes.Equal(hash1.Sum(nil), hash2.Sum(nil)), nil
 }
+
+func execCommand(t testing.TB, command string, args ...string) {
+	cmd := exec.Command(command, args...)
+	output, err := cmd.CombinedOutput()
+	require.NoError(t, err, "failed to execute command %s: %s", cmd.String(), string(output))
+}
